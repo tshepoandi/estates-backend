21da6eed55367e443e438d4725ab0e7975d9190d
tshepoandi <lovekilledtshepo@gmail.com>
2023-10-11 10:32:58 +0200

    encryot password

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,10 +1,17 @@
+import bcryptjs from 'bcryptjs';
 import User from "../models/user.model.js"
 
 export const signup = async(req, res) => {
     const { username, email, password } = req.body;
-    console.log(req.body)
-    const newUser = new User({ username, email, password })
-    await newUser.save().then(() => {
-        res.status(201).json("User Created ")
-    })
+    const hashedPassword = bcryptjs.hashSync(password, 10)
+    const newUser = new User({ username, email, password: hashedPassword })
+    try {
+        await newUser.save().then(() => {
+            res.status(201).json("User Created ")
+        })
+    } catch (error) {
+        res.status(500).json(error.message);
+    }
+
+
 }
